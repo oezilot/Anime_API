30527295e5d0287c28c1862fdfd16321aafcd720
zoe <zoe.flumini@gmail.com>
2024-10-16 14:37:27 +0200

    new project from scratch

--- app.py
+++ app.py
@@ -15,19 +15,6 @@ def index():
     return render_template('index.html')
 
 
-# this handles the API call with the info from javascript
-# Route to search for anime using the Jikan API
-@app.route('/search_anime', methods=['GET'])
-def search_anime():
-    anime_name = request.args.get('anime')  # Get anime name from the query string
-    api_url = f"https://api.jikan.moe/v4/anime?q={anime_name}"
-
-    # Make the API request
-    response = requests.get(api_url)
-    anime_data = response.json()  # Parse the JSON response
-
-    return jsonify(anime_data)  # Return the data as JSON to the frontend, converts python data into jason format which javascript will receive
-
 
 if __name__ == '__main__':
     app.run(debug=True)
