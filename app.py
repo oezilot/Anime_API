90180e1f7dbd0686b9724446309b42e385f2476c
zoe <zoe.flumini@gmail.com>
2024-10-16 16:28:46 +0200

    comments

--- app.py
+++ app.py
@@ -6,31 +6,31 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def search():
     
-    anime_results = None
+    anime_results = None # liste mit den animes die ausgegeben werden
 
     anime_name = request.form.get('searchbar')  # Get the name the user searched for in the search bar
     
-    # if search bar has a value
+    # falls etwas in die searchbar submittet wurde...
     if anime_name:
         api_name = f"https://api.jikan.moe/v4/anime?q={anime_name}"
         response_name = requests.get(api_name)
 
         if response_name.status_code == 200:
             anime_data = response_name.json()  # Parse JSON response
-            anime_results = anime_data['data']  # Contains all anime results with the name
+            anime_results = anime_data['data']  # enthält alle animes mit dem namen der searchbar
             print(anime_results[1]['title'])
             return render_template('results.html', anime_results=anime_results)
         else:
             return "Error fetching data from API", 500
 
-
-    else:  # if search bar is empty, display all anime
+    # falls nichts in die searchbar submittt wurde...
+    else:  
         api_all = f"https://api.jikan.moe/v4/anime"
         response_all = requests.get(api_all)
 
         if response_all.status_code == 200:
             anime_data = response_all.json()  # Parse JSON response
-            anime_results = anime_data['data']  # Contains all anime results
+            anime_results = anime_data['data']  # enthält alle anime überhaupt
             print(anime_results[2]['title'])
             return render_template('results.html', anime_results=anime_results)
         else:
