968597d436927a05e37cc3a32acdc61d955afc9e
zoe <zoe.flumini@gmail.com>
2024-10-18 11:04:16 +0200

    sortierlink type (chatgpt)

--- loadmore.py
+++ loadmore.py
@@ -3,15 +3,27 @@ import requests
 
 app = Flask(__name__)
 
+
 # function for fetching the data with the api for all anime for a specific page
-def fetch_all(page_number):
+def fetch_all(page_number, anime_type=None):
+
+    # this is the default url without using any of the parameters! it displays all anime in pages
     api_page = f"https://api.jikan.moe/v4/anime?page={page_number}"
+
+    # this checks wether a button was clicked
+    if anime_type:
+        api_page = f"https://api.jikan.moe/v4/anime?page={page_number}&type={anime_type}"
+
+    # here the data gets fetches from the API defined at the top with or without parameters
     response = requests.get(api_page)
 
+    # make data ready to be used in the html (dejasonify and select the data-list)
     if response.status_code == 200:
         data_json = response.json()  # Parse JSON response
         data = data_json['data']
         return data
+    else:
+        return []
 # print(fetch_all(1))
 
 
@@ -20,9 +32,13 @@ def fetch_all(page_number):
 @app.route('/', methods=['GET', 'POST'])
 def all_anime():
     page_number = int(request.args.get('page', 1)) # mit dem 'befehl' kriegt man die page auf der man gerade drauf ist heraus, falls nichts vorhanden ist dann wird es default 1 gesetzt
+    anime_type = None # per default the type is none
 
     # navigation_buttons event
     if request.method == 'POST':
+        # get the anime_type from the form
+        anime_type = request.form.get('type')
+
         navigation_action = request.form.get('navigation')  # navigation ist der name der navigationbuttons
 
         if navigation_action == 'previous':
@@ -31,9 +47,13 @@ def all_anime():
             page_number = page_number + 1
 
         # Redirect to the new page with updated page_number, wenn man ja den previous button clickt dann redirectet es ja einem zu der page auf der man bereits einmal war!
-        return redirect(url_for('all_anime', page=page_number))
+        return redirect(url_for('all_anime', page=page_number, type=anime_type))
 
-    data = fetch_all(page_number)
+    # If it's a GET request, get the type filter from the URL
+    anime_type = request.args.get('type')
+
+    # which data will be displayed, with which paramteers included and which page?
+    data = fetch_all(page_number, anime_type)
     
     return render_template('loadmore.html', data=data, page_number=page_number)
 
