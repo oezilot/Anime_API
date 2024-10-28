6e6fb099d66c66ce5146c16be62a64f2ea76fc75
zoe <zoe.flumini@gmail.com>
2024-10-28 12:02:25 +0100

    image test (not passed)

--- tests/test_selber.py
+++ tests/test_selber.py
@@ -8,12 +8,32 @@ def test_home_page(client):
     assert response.status_code == 200
 
 
-# check if homepage render expected content
+# check if homepage render expected content (mal nur den content und nicht schon die funktionalität der elemente prüfen)
 def test_homepage_content(client):
     response = client.get('/')
 
     # Check if the content includes specific text
     assert b"Anime Search" in response.data # b stands for bytes
+
+    # checks the resence of the form-elements (the 5 different parameters)
+    assert b'<input type="text" name="parameter_title"' in response.data
+    assert b'<select name="parameter_genre"' in response.data
+    assert b'<select name="parameter1"' in response.data
+    assert b'<select name="parameter2"' in response.data
+    assert b'<select name="parameter3"' in response.data
+
+    # Check for anime data placeholders
+    assert b'Title:' in response.data
+    assert b'Type:' in response.data
+    assert b'Status:' in response.data
+    assert b'Rating:' in response.data
+    assert b'Genre:' in response.data
+    #checking for images doesnt work because of the dynamic image.url?!
+    #assert b'<img src=""' in response.data
+    #assert b'images.jpg.image_url' in response.data  # Checks that the specific part of the image URL is included
+
+
+
 
 
 
