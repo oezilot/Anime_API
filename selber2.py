1567ac3a03564a0833b81c6cf5e623001adbe41c
ozilot <zoe.flumini@gmail.com>
2024-11-01 17:25:15 +0100

    wip

--- selber2.py
+++ selber2.py
@@ -29,20 +29,20 @@ Form (anpassung der items in der session)
 
 from flask import Flask
 import secrets # für die generiereung eines secret_keys
+import requests
 
 app2 = Flask(__name__) # mit diser variable wird die app definiert und dass es sich um eine flask app handelt; man könnte alternativ auch einen andere namen verwenden: "app=Flask(__IrgendEinName__)"
 app2.secret_key = secrets.token_hex(16) # ein zufälliger key wird jedes mal generiert, das wird benötigt für die session
 
-''' 
-nur zum testen:
+
+#nur zum testen:
 
 params = {
-    "param1": "type", 
-    "param2": 3
+    "type": "tv", 
+    "q": "naruto"
 }
-page = 500
+page = 1
 
-'''
 
 #=================== urlBuilder-Functions (3) =====================
 def url_animes(page, params):
@@ -62,6 +62,32 @@ def url_characters(anime_id):
     api_url = f"https://api.jikan.moe/v4/anime/{anime_id}/characters"  # path parameter
     print(f"URL characters:{api_url}")
     return api_url
+
+
+def fetch_data(page, params):
+    try: # den call probieren zu machen
+        # antwort auf den api-call mit dem api-url der url_animes-funktion (fetchen)
+        response_animes = requests.get(url_animes(page, params))
+        if response_animes.status_code == 200:
+            animes_dict = response_animes.json() #JSON-dten in eine variable laden (= Dictionary)
+            if "data" in animes_dict and animes_dict["data"]: # überprüüft ob der key namens data im dict vorhanden ist und ob dieser key einen value hat
+                animes_data = animes_dict.get('data', []) # data ist eine liste mit einem dictionary
+                print(f"ERFOLG FETCHING animes_data: {animes_data}")
+                return animes_data # der rückgabewert der funktion sind die daten des calls
+            else:
+                print("ERROR FETCHING animes_data: es existiern keine Anime-Daten")
+                return None
+        else: # falls der call nicht erfolgreich war (400: bad request)
+            print(f"ERROR FETCHING animes_data:{response_animes.status_code}")
+            return None
+    except requests.RequestException as e: # falls der call selbst fehlschlägt (das mit dem exception ist so was speziellen für api-calls)
+        print(f"ERROR MAING THE API CALL:{e}")
+        return None
+    
+# fetch-funktion aufrufen: (erwarteter output = api-url und die daten)
+fetch_data(page, params)
+url_animes(page, params)
+
 
 
 # das besagt dass dieses file das main file und nicht nur irgendein modul ist (nur da main file wird gerunnt!)
