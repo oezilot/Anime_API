936e42ad3d68fc71ae385cd708c7e18f653207f6
ozilot <zoe.flumini@gmail.com>
2024-11-04 12:27:24 +0100

    anime und characters displayen

--- selber2.py
+++ selber2.py
@@ -36,6 +36,7 @@ app2.secret_key = secrets.token_hex(16) # ein zufälliger key wird jedes mal gen
 
 
 #nur zum testen:
+# TEST: testwerte um requests etc zu simulieren
 
 params = {
     "type": "tv", 
@@ -48,6 +49,7 @@ error = None # diese variable überbringt dem html immer den error zum darstelle
 
 
 #=================== URL-Builder-Functions (3) =====================
+# TEST: werden die urls richtig gebildet?
 # erwartender output = API url mit den richtigen parametern
 def url_animes(page, params):
     api_url = f"https://api.jikan.moe/v4/anime?page={page}" # das ist ein query parameter für filtering sachen
@@ -121,14 +123,14 @@ def fetch_characters(anime_id):
             characters_dict = response_characters.json() #JSON-daten in eine variable laden (= Dictionary)
             if "data" in characters_dict and characters_dict["data"]: # überprüüft ob der key namens data im dict vorhanden ist und ob dieser key einen value hat (hier ist data eine liste!)
                 characters_data = characters_dict.get('data', []) 
-                print(f"ERFOLG FETCHING character_data: {characters_data}")
+                print(f"ERFOLG FETCHING characters_data: {characters_data}")
                 return characters_data # der rückgabewert der funktion sind die daten des calls
             else:
                 # wenn keine daten existieren zu den gewählten parametern!
                 print("ERROR FETCHING character_data: es existiern keine Anime-Daten")
                 return None
         else: # falls der call nicht erfolgreich war (400: bad request), zum bsp wenn dieanime_id nicht existiert!
-            print(f"ERROR FETCHING animes_data:{response_characters.status_code}")
+            print(f"ERROR FETCHING characters_data:{response_characters.status_code}")
             return None
     except requests.RequestException as e: # falls der call selbst fehlschlägt (das mit dem exception ist so was speziellen für api-calls)
         print(f"ERROR MAKING THE Characters-API CALL:{e}")
@@ -137,12 +139,20 @@ def fetch_characters(anime_id):
 #=================== Fetch-Funktionen testen mit vorgegebenen dictionary, page, anime_id (3) =====================
 # fetch_animes(page, params) # (erwarteter output = api-url und die daten aller animes)
 # fetch_anime(anime_id) # url and daten zu einem bestimmten anime
+'''
+anime_data = fetch_anime(anime_id)  # Anime-Daten abrufen
+if anime_data is not None:
+    print("image_url:", anime_data['images']['webp']['image_url'])  # mal_id ausgeben
+else:
+    print("Fehler: fetch_anime() hat None zurückgegeben.")
+'''
 # fetch_characters(anime_id) # url and daten zu allen charaktere eines bestimmten animes
 
-'''
+
 #=================== Pages/Routen (3) =====================
-# TESTS: bsp mit daten enthalten und bp mit keinen daten wenn die parameter kein erfolgreiches resultat ausspucken!
+# TESTS: bsp mit daten enthalten und bp mit keinen daten wenn die parameter kein erfolgreiches resultat ausspucken!, alle sachen die man displayed haben will teste wie image, title etc
 # daten displayen im html, alles was diese funktion returnt wird im html angezeigt!
+
 @app2.route('/', methods=['GET'])
 def display_animes_data():
     global page, params
@@ -151,8 +161,29 @@ def display_animes_data():
         return render_template('selber2.html', animes_data=animes_data)
     else:
         # diese zeile hier wird im html angezeigt und nicht im terminal!
-        return "ERROR in der Display funktion: fetch_data gibt None zurück!"
-'''
+        return "ERROR in der Display funktion: fetch_animes gibt None zurück!"
+    
+@app2.route('/anime')
+def display_anime_data():
+    global anime_id
+    if fetch_anime(anime_id) != None: # hier könnte es auch sein dass animes_data none ist deshalb muss man das noch überprüfen
+        anime_data = fetch_anime(anime_id) 
+        return render_template('anime2.html', anime_data=anime_data)
+    else:
+        # diese zeile hier wird im html angezeigt und nicht im terminal!
+        return "ERROR in der Display funktion: fetch_anime gibt None zurück!"
+
+@app2.route('/characters')
+def display_characters_data():
+    global anime_id
+    if fetch_anime(anime_id) != None: # hier könnte es auch sein dass animes_data none ist deshalb muss man das noch überprüfen
+        characters_data = fetch_characters(anime_id) 
+        return render_template('characters2.html', characters_data=characters_data)
+    else:
+        # diese zeile hier wird im html angezeigt und nicht im terminal!
+        return "ERROR in der Display funktion: fetch_characters gibt None zurück!"
+
+
 
 
 
