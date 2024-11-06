a2ff92162dee2428cb9f46bfe46f13b8b49b458d
ozilot <zoe.flumini@gmail.com>
2024-11-06 16:14:32 +0100

    wip

--- tests2/test_integration2.py
+++ tests2/test_integration2.py
@@ -12,8 +12,19 @@ def client():
 
 
 def test_update_session():
-    
+    # simulate data for the POST-request
+    form_data = {
+        "q":"naruto",
+        "genres":"1"
+    }
 
-# simulate POST-request
+    # simulate the POST-request: der client postet doe form_data an die funktion update_session
+    response = client.post('update_session', data=form_data, follow_redicrects=True)
 
-# check if the session was correclty updated
+    # check if the session was correclty updated (with client)
+    with client.session_transaction() as sess:
+        assert sess['params']['q'] == "naruto"
+        assert sess['params']['genres'] == "1"
+
+    # verify the reset
+    assert response.status_code == 200
